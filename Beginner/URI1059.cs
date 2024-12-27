7969586f224c67d20c6da9d5478a7436f4f59a6c
Daniel Santos <danielsan171817@gmail.com>
2024-12-27 19:59:58 -0300

    Create URI1059.cs

---
+++ Beginner/URI1059.cs
@@ -0,0 +1,12 @@
+using System;
+
+class URI1059
+{
+    static void Main(string[] args)
+    {   //Demorei pra entender o statement condicional
+        for(int i = 1; i <= 100; i++){
+            if(i % 2 == 0){
+            Console.WriteLine(i);}
+        }
+    }
+}
